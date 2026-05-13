a3a97bba200102b4cdc99fae0d01f3b950473e88
khaledosama <khaledosamaselem13@gmail.com>
2026-05-14 00:48:14 +0300

    Project v2

--- Non-Recursive.c
+++ Non-Recursive.c
@@ -1,28 +1,21 @@
 #include <stdio.h>
+#include <stdlib.h>
 
-void findMaxAndCount(int candles[], int age, int index, int *maximum, int *count){
+int birthdayCakeCandlesNonRecursive(int candles[], int age){
+    int Maximum = candles[0];
+    int count = 1;
 
-    if (index == age){
-        return;
+    for(int i = 1; i < age; i++){
+
+        if(candles[i] > Maximum){
+            Maximum = candles[i];
+            count = 1;
+        }
+
+        else if(candles[i] == Maximum){
+            count++;
+        }
     }
-
-    if (candles[index] > *maximum){
-        *maximum = candles[index];
-        *count = 1;
-    }
-
-    else if (candles[index] == *maximum){
-        (*count)++;
-    }
-
-    findMaxAndCount(candles, age, index + 1, maximum, count);
-}
-
-int birthdayCakeCandles(int candles[], int age){
-    int maximum = candles[0];
-    int count = 0;
-
-    findMaxAndCount(candles,age, 0, &maximum, &count);
 
     return count;
 }
@@ -31,18 +24,15 @@ int main()
 {
     int age;
     printf("Please enter the age: ");
-    scanf("%d", &age);
+    scanf("%d",&age);
 
-    int candles[age];
-
-    for (int i = 0; i < age; i++){
-        printf("Please enter the candle number %d: ", i + 1);
-        scanf("%d", &candles[i]);
+    int candlesHeights[age];
+    for(int i=0;i<age;i++){
+        printf("Please enter the candle number %d: ",i+1);
+        scanf("%d",&candlesHeights[i]);
     }
 
-     int CountOfMaximumHeight= birthdayCakeCandles(candles, age);
-
-    printf("the maximum height has appeared %d times\n", CountOfMaximumHeight);
-
-    return 0;
+    int CountOfMaximumHeight=birthdayCakeCandlesNonRecursive(candlesHeights,age);
+    printf("the maximum height has appeared %d times",CountOfMaximumHeight);
 }
+
--- Recursive.c
+++ Recursive.c
@@ -1,20 +1,28 @@
 #include <stdio.h>
-#include <stdlib.h>
-int birthdayCakeCandlesRecursive(int candles[], int age){
-    int Maximum = candles[0];
-    int count = 1;
 
-    for(int i = 1; i < age; i++){
+void findMaxAndCount(int candles[], int age, int index, int *maximum, int *count){
 
-        if(candles[i] > Maximum){
-            Maximum = candles[i];
-            count = 1;
-        }
-
-        else if(candles[i] == Maximum){
-            count++;
-        }
+    if (index == age){
+        return;
     }
+
+    if (candles[index] > *maximum){
+        *maximum = candles[index];
+        *count = 1;
+    }
+
+    else if (candles[index] == *maximum){
+        (*count)++;
+    }
+
+    findMaxAndCount(candles, age, index + 1, maximum, count);
+}
+
+int birthdayCakeCandles(int candles[], int age){
+    int maximum = candles[0];
+    int count = 0;
+
+    findMaxAndCount(candles,age, 0, &maximum, &count);
 
     return count;
 }
@@ -23,14 +31,18 @@ int main()
 {
     int age;
     printf("Please enter the age: ");
-    scanf("%d",&age);
+    scanf("%d", &age);
 
-    int candlesHeights[age];
-    for(int i=0;i<age;i++){
-        printf("Please enter the candle number %d: ",i+1);
-        scanf("%d",&candlesHeights[i]);
+    int candles[age];
+
+    for (int i = 0; i < age; i++){
+        printf("Please enter the candle number %d: ", i + 1);
+        scanf("%d", &candles[i]);
     }
 
-    int CountOfMaximumHeight=birthdayCakeCandlesRecursive(candlesHeights,age);
-    printf("the maximum height has appeared %d times",CountOfMaximumHeight);
+     int CountOfMaximumHeight= birthdayCakeCandles(candles, age);
+
+    printf("the maximum height has appeared %d times\n", CountOfMaximumHeight);
+
+    return 0;
 }
